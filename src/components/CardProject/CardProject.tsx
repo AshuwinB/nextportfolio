829fd9682c2b715d3eb8c7426a229e1dc27af0e4
RishiBose961 <rishibose1901@gmail.com>
2023-07-17 22:19:40 -0700

    auth

--- src/components/CardProject/CardProject.tsx
+++ src/components/CardProject/CardProject.tsx
@@ -8,7 +8,6 @@ import { CiEdit } from "react-icons/ci";
 import Link from "next/link";
 import Image from "next/image";
 
-
 interface ButtonProps {
   mutate: (params: any) => any;
 }
@@ -16,7 +15,7 @@ interface ButtonProps {
 const CardProject = () => {
   const session = useSession();
   const fetcher = (url: any) => fetch(url).then((res) => res.json());
-  const { data,mutate, error, isLoading } = useSWR(
+  const { data, mutate, error, isLoading } = useSWR(
     `/api/posts?username=${session?.data?.user?.email}`,
     fetcher
   );
@@ -27,13 +26,16 @@ const CardProject = () => {
   return (
     <div className="container mx-auto px-4">
       <div className="flex justify-between mt-4">
-      <p className=" text-xl font-semibold">View Project</p>
+        <p className=" text-xl font-semibold">View Project</p>
         <ModelProje mutate={mutate} />
       </div>
-      
+
       <div className=" grid grid-cols-1 md:col-span-2 gap-4 lg:grid-cols-2 mb-4">
-        {data.map((item: any,index:number) => (
-          <div key={index} className="max-w-sm hover:bg-stone-200 bg-white border mt-5 h-fit border-gray-200 rounded-lg shadow  dark:border-gray-700">
+        {data.map((item: any, index: number) => (
+          <div
+            key={index}
+            className="max-w-sm hover:bg-stone-200 bg-white border mt-5 h-fit border-gray-200 rounded-lg shadow  dark:border-gray-700"
+          >
             <div className="flex justify-center pt-4">
               <Image src={item.imageurl} alt="" width={500} height={500} />
             </div>
@@ -44,28 +46,30 @@ const CardProject = () => {
                   {item.title}
                 </h5>
               </a>
-              <p className="mb-3 font-normal text-gray-700 ">
-                {item.about}.
-              </p>
-            
-                <div className=" flex justify-between mt-1 mb-1 cursor-pointer">
+              <p className="mb-3 font-normal text-gray-700 ">{item.about}.</p>
+
+              <div className=" flex justify-between mt-1 mb-1 cursor-pointer">
+                {session.status === "authenticated" ? (
                   <Link href={item.githuburl}>
                     {" "}
                     <BsGithub className="h-6 w-6 text-black" />
                   </Link>
+                ) : (
+                  ""
+                )}
 
-                  <Link href={item.websiteurl}>
-                    <div className=" flex justify-center space-x-2">
-                      <BsGlobe className="h-6 w-6 text-blue-500" />{" "}
-                      <p>{item.type}</p>
-                    </div>
-                  </Link>
-                  {
-                    session.status ==="authenticated"? <CiEdit className="h-6 w-6 text-blue-500" />:""
-                  }
-                 
-                </div>
-             
+                <Link href={item.websiteurl}>
+                  <div className=" flex justify-center space-x-2">
+                    <BsGlobe className="h-6 w-6 text-blue-500" />{" "}
+                    <p>{item.type}</p>
+                  </div>
+                </Link>
+                {session.status === "authenticated" ? (
+                  <CiEdit className="h-6 w-6 text-blue-500" />
+                ) : (
+                  ""
+                )}
+              </div>
             </div>
           </div>
         ))}
